30cf6de7a5976b286a7ab100a09dcea4821b1d9e
paulohldev <paulo85542080@gmail.com>
2022-08-13 20:58:01 -0300

    front end e back end funcionando

--- database/config.js
+++ database/config.js
@@ -1,12 +1,12 @@
 // get the client
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'ifpe1234',
-  database: 'locadoraveiculos',
+  host: "127.0.0.1",
+  user: "root",
+  password: "admin",
+  database: "veiculos2",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- database/dao.js
+++ database/dao.js
@@ -1,26 +1,57 @@
 const pool = require("./config");
 
 let operations = {
-  list: function() {
-      return pool.promise().query('select * from veiculos')
+  list: function () {
+    return pool.promise().query("select * from veiculos");
   },
   findById(id) {
-      return pool.promise().query('select * from veiculos where id = ?', [id])
+    return pool.promise().query("select * from veiculos where id = ?", [id]);
   },
-  save: function(veiculo) {
-      return pool.promise().execute('insert into veiculos (marca, modelo, ano, cor, categoria, combustivel, cambio, pcd) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', 
-      [veiculo.marca, veiculo.modelo, veiculo.ano, veiculo.cor, veiculo.categoria, veiculo.combustivel, veiculo.cambio, veiculo.pcd])
+  save: function (veiculo) {
+    return pool
+      .promise()
+      .execute(
+        "insert into veiculos (marca, modelo, ano, cor, categoria, combustivel, cambio, pcd) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          veiculo.marca,
+          veiculo.modelo,
+          veiculo.ano,
+          veiculo.cor,
+          veiculo.categoria,
+          veiculo.combustivel,
+          veiculo.cambio,
+          veiculo.pcd,
+        ]
+      );
   },
-  update: function(veiculo) {
-      return pool.promise().execute('update veiculos set marca = ?, modelo = ?, ano = ?, cor = ?, categoria = ?, combustivel = ?, cambio = ?, pcd = ? where id = ?',
-      [veiculo.marca, veiculo.modelo, veiculo.ano, veiculo.cor, veiculo.categoria, veiculo.combustivel, veiculo.cambio, veiculo.pcd, veiculo.id])
+  update: function (veiculo) {
+    return pool
+      .promise()
+      .execute(
+        "update veiculos set marca = ?, modelo = ?, ano = ?, cor = ?, categoria = ?, combustivel = ?, cambio = ?, pcd = ? where id = ?",
+        [
+          veiculo.marca,
+          veiculo.modelo,
+          veiculo.ano,
+          veiculo.cor,
+          veiculo.categoria,
+          veiculo.combustivel,
+          veiculo.cambio,
+          veiculo.pcd,
+          veiculo.id,
+        ]
+      );
   },
-  remove: function(id) {
-      return pool.promise().execute('delete from veiculos where id = ?', [id])
+  remove: function (id) {
+    return pool.promise().execute("delete from veiculos where id = ?", [id]);
   },
-  search: function(modelo) {
-      return pool.promise().query('select * from veiculos where modelo like ?', ['%'+modelo+'%'])
-  }
+  search: function (modelo) {
+    return pool
+      .promise()
+      .query("select * from veiculos where modelo like ?", [
+        "%" + modelo + "%",
+      ]);
+  },
 };
 
 module.exports = operations;
